bacf0944ea0483358ca1fef13abb0e400e3a8078
Alexey Kozlov <kozlov.alex.dev@gmail.com>
2022-06-26 16:46:05 +0300

    init commit

---
+++ src/main/java/com/studyhardpro/board/forum/BoardForumApplication.java
@@ -0,0 +1,13 @@
+package com.studyhardpro.board.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studyhardpro/board/forum/BoardForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyhardpro.board.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
